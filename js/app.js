80ff113222b94744ab5e1ec5e6e24ab2d1d7be04
aplloshahriar <aaronshahriar.as@gmail.com>
2023-03-02 14:39:17 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -44,7 +44,9 @@ const displayPhones=(phones,dataLimit)=>{
                   <h5 class="card-title">${phone.phone_name}</h5>
                   <p class="card-text">This is a longer card with supporting         
 text below as a natural lead-in to additional content. This content is a little bit longer.</p> 
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show DEtails</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phonemodal">Show DEtails</button>
+
+               
                 </div>
               </div>
 `;
@@ -104,6 +106,13 @@ const loadPhoneDetails=async id=>{
     const url=`https://openapi.programming-hero.com/api/phone/${id}`;
     const res=await fetch(url);
     const data= await res.json();
-    console.log(data.data);
+    displayPhoneDetails(data.data);
 }
-// loadPhones();
+
+const displayPhoneDetails=phone=>{
+    console.log(phone);
+    const modalTitle=document.getElementById('phonemodalLabel');
+    modalTitle.innerText=phone.name;
+
+}
+loadPhones('apple');
